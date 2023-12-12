035db2e46296a1a2506306b800c3d28e58843d93
ahmad-666 <ahmadrezagghz666@gmail.com>
2023-12-12 12:08:29 +0330

    feat: add client services for companies,emojis

--- src/services/company.ts
+++ src/services/company.ts
@@ -1,7 +1,35 @@
-import type { GetCompaniesArgs } from "@/src/types/Company";
+import axios from "@/src/utils/axios";
+import type {
+  GetCompaniesResponse,
+  GetCompanyResponse,
+  GetIndustriesResponse,
+  GetCompaniesArgs,
+  GetCompanyArgs,
+} from "@/src/types/Company";
 
-export const getCompanies = ({
-  page = 1,
-  pageSize = 8,
+export const getCompanies = async ({
   industry,
-}: GetCompaniesArgs) => {};
+  page = 1,
+  pageSize,
+}: GetCompaniesArgs) => {
+  const { data } = await axios.get<GetCompaniesResponse>("/companies", {
+    params: {
+      industry,
+      page,
+      pageSize,
+    },
+  });
+  return data;
+};
+export const getCompany = async ({ companyId }: GetCompanyArgs) => {
+  const { data } = await axios.get<GetCompanyResponse>(
+    `/companies/${companyId}`
+  );
+  return data;
+};
+export const getIndustries = async () => {
+  const { data } = await axios.get<GetIndustriesResponse>(
+    "/companies/industries"
+  );
+  return data;
+};
--- src/services/emoji.ts
+++ src/services/emoji.ts
@@ -1,8 +1,35 @@
-import type { GetEmojisArgs } from "@/src/types/Emoji";
+import axios from "@/src/utils/axios";
+import type {
+  GetEmojiArgs,
+  GetEmojisArgs,
+  GetEmojiResponse,
+  GetEmojisResponse,
+  GetEmojiCategoriesResponse,
+} from "@/src/types/Emoji";
 
-export const getEmojis = ({
-  page = 1,
-  pageSize = 8,
-  category,
+export const getEmoji = async ({ emojiId }: GetEmojiArgs) => {
+  const { data } = await axios.get<GetEmojiResponse>(`/emojis/${emojiId}`);
+  return data;
+};
+export const getEmojis = async ({
   urls,
-}: GetEmojisArgs) => {};
+  category,
+  page = 1,
+  pageSize,
+}: GetEmojisArgs) => {
+  const { data } = await axios.get<GetEmojisResponse>(`/emojis`, {
+    params: {
+      urls: urls?.join(","),
+      category,
+      page,
+      pageSize,
+    },
+  });
+  return data;
+};
+export const getEmojiCategories = async () => {
+  const { data } = await axios.get<GetEmojiCategoriesResponse>(
+    "/emojis/categories"
+  );
+  return data;
+};
