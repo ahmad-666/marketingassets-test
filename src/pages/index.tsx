13d99f7d88b50b136391548724c66d9255ce641b
ahmad-666 <ahmadrezagghz666@gmail.com>
2023-12-14 13:59:20 +0330

    feat: add pagination component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import Pagination from "@/src/components/common/Pagination";
+import { useState } from "react";
+
 export default function Page() {
+  const [page, setPage] = useState(1);
   return (
     <div>
       <h1>Home</h1>
+      <Pagination
+        page={page}
+        setPage={(newVal) => setPage(newVal)}
+        totalItems={100}
+        pageSize={10}
+      />
     </div>
   );
 }
