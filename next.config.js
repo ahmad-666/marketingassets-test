4f77b32ab1ba716c6538268ecb5772e68f06cc90
ahmad-666 <ahmadrezagghz666@gmail.com>
2023-12-19 12:39:29 +0330

    feat: remove trailing slash

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: "dist",
-  trailingSlash: true,
+  //trailingSlash: true,
   experimental: {
     serverComponentsExternalPackages: ["sequelize"],
   },
