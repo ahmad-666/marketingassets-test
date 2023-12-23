4126a1d9dd4f8c57713f8c1f8994985e45044cf6
ahmad-666 <ahmadrezagghz666@gmail.com>
2023-12-23 16:06:16 +0330

    feat: start to add hub section

--- src/components/others/HubSection.tsx
+++ src/components/others/HubSection.tsx
@@ -1,5 +1,106 @@
-import React from "react";
+import { useMemo, useState } from "react";
+import LineTitle from "@/src/components/others/LineTitle";
+import Image from "next/image";
+import useHover from "@/src/hooks/useHover";
+import styles from "./hubSection.module.scss";
 
-export default function HubSection() {
-  return <div>HubSection</div>;
+type HubSectionProps = {
+  className?: string;
+};
+type Item = {
+  id: number;
+  text: string;
+  imgSrc: string;
+  className?: string;
+};
+
+const items: Item[] = [
+  {
+    id: 1,
+    text: "Unleash Your Brand's Unique Personality with CUFinder.",
+    imgSrc: "/images/hub/hub-1.svg",
+    className: "left-0 top-0",
+  },
+  {
+    id: 2,
+    text: "Elevate Your Marketing with CUFinder's Custom Assets.",
+    imgSrc: "/images/hub/hub-2.svg",
+    className: "right-0 top-0",
+  },
+  {
+    id: 3,
+    text: "Unlock Your Brand's Potential with CUFinder's Assets.",
+    imgSrc: "/images/hub/hub-3.svg",
+    className: "left-0 bottom-0",
+  },
+  {
+    id: 4,
+    text: "CUFinder: Your Key to Successful Marketing.",
+    imgSrc: "/images/hub/hub-4.svg",
+    className: "right-0 bottom-0",
+  },
+  {
+    id: 5,
+    text: "Get Noticed with CUFinder's Custom Emojis, Logos, Gifs, and Stickers.",
+    imgSrc: "/images/hub/hub-5.svg",
+    className: "left-50 top-50",
+  },
+  {
+    id: 6,
+    text: "Make a Lasting Impression with CUFinder's Customized Marketing Solutions.",
+    imgSrc: "/images/hub/hub-6.svg",
+    className: "right-50 top-50",
+  },
+];
+export default function HubSection({ className = "" }: HubSectionProps) {
+  const [activeId, setActiveId] = useState(1);
+  const activeItem = useMemo(() => {
+    return items.find((item) => item.id === activeId);
+  }, [activeId]);
+  return (
+    <div className={`${className}`}>
+      <LineTitle title="Hub of Marketing Assets" />
+      <div className="position-relative mt-3">
+        <Image
+          src="/images/hub/hub-bg.png"
+          alt="hub"
+          width={750}
+          height={750}
+          className="w-100 h-100 position-absolute left-0 top-0 z-1"
+        />
+        <div className="position-relative z-2 d-flex justify-content-center align-items-center">
+          {items.map((item) => (
+            <div
+              key={item.id}
+              className={`position-absolute rounded-circle overflow-hidden ${styles.item} ${item.className}`}
+            >
+              <Image
+                src={item.imgSrc}
+                alt={item.imgSrc}
+                width={200}
+                height={200}
+                className="w-100 h-100"
+              />
+            </div>
+          ))}
+          {activeItem && (
+            <div className="d-flex flex-column align-items-center">
+              <div
+                className={`overflow-hidden rounded-circle ${styles["active-item"]}`}
+              >
+                <Image
+                  src={activeItem.imgSrc}
+                  alt={activeItem.imgSrc}
+                  width={200}
+                  height={200}
+                  className="w-100 h-100"
+                />
+              </div>
+              <h3 className="fs-4 text-dark-color mt-4">{activeItem.text}</h3>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import MetaData from "@/src/components/common/MetaData";
 import SectionContainer from "@/src/components/common/SectionContainer";
 import AssetsList from "@/src/components/asset/AssetsList";
 import EmojiCategoryList from "@/src/components/emoji/EmojiCategoriesList";
+import HubSection from "@/src/components/others/HubSection";
 import FaqsList from "@/src/components/others/FaqsList";
 import BrandsList from "@/src/components/brand/BrandsList";
 import NewsLetters from "@/src/components/others/NewsLetters";
@@ -13,7 +14,10 @@ export default function Page() {
       <SectionContainer>
         <AssetsList />
         <EmojiCategoryList className="mt-7" />
-        <FaqsList className="mt-7" />
+      </SectionContainer>
+      {/* <HubSection className="mt-7" /> */}
+      <SectionContainer className="mt-7">
+        <FaqsList />
         <BrandsList className="mt-7" />
       </SectionContainer>
       <NewsLetters className="mt-5 mb-n5" />
