dc3a02688690ea0e7eca46e35cca2514c62b15f0
ahmad-666 <ahmadrezagghz666@gmail.com>
2023-12-25 13:37:52 +0330

    feat: add rating component

--- src/components/common/Rating.tsx
+++ src/components/common/Rating.tsx
@@ -1,5 +1,42 @@
-import React from "react";
+import { useCallback, type ComponentProps } from "react";
+import { Rating as ReactRating } from "react-simple-star-rating";
 
-export default function Rating() {
-  return <div>Rating</div>;
+type RatingProps = {
+  value: number;
+  onChange?: (newValue: number) => void;
+  ratingClassName?: string;
+  className?: string;
+} & Omit<
+  ComponentProps<typeof ReactRating>,
+  "initialValue" | "onClick" | "className"
+>;
+export default function Rating({
+  //out own props:
+  value,
+  onChange,
+  ratingClassName = "",
+  className = "",
+  //react-simple-star-rating props:
+  //iconsCount,size,allowFraction(for half increment),allowHover,readonly,rtl
+  //fillIcon,fillColor,emptyIcon,emptyColor,style
+  //showTooltip,tooltipDefaultText,tooltipClassName,tooltipStyle
+  //onPointerMove,onPointerEnter,onPointerLeave
+  ...rest
+}: RatingProps) {
+  const clickHandler = useCallback(
+    (newValue: number) => {
+      if (onChange) onChange(newValue);
+    },
+    [onChange]
+  );
+  return (
+    <div className={`${className}`}>
+      <ReactRating
+        initialValue={value}
+        onClick={clickHandler}
+        className={`${ratingClassName}`}
+        {...rest}
+      />
+    </div>
+  );
 }
