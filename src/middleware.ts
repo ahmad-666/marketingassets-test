8c4bb646d9a71b5bd2d0ffa6be5f1041837404d9
ahmad-666 <ahmadrezagghz666@gmail.com>
2023-12-27 13:32:47 +0330

    fix: fix middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,20 @@
-// import { type NextRequest, NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
-// export function middleware(request: NextRequest) {
-//   if (request.method === "OPTIONS") {
-//     //OPTIONS is for preflight request of CORS and we should handle it and for all OPTIONS request we should return response
-//     return NextResponse.json(
-//       {},
-//       {
-//         status: 200,
-//         //headers:{}
-//       }
-//     );
-//     //we can also use: return new NextResponse("", {status: 200,headers:{}});
-//   }
-//   return NextResponse.next();
-// }
+export function middleware(request: NextRequest) {
+  // if (request.method === "OPTIONS") {
+  //   //OPTIONS is for preflight request of CORS and we should handle it and for all OPTIONS request we should return response
+  //   return NextResponse.json(
+  //     {},
+  //     {
+  //       status: 200,
+  //       //headers:{}
+  //     }
+  //   );
+  //   //we can also use: return new NextResponse("", {status: 200,headers:{}});
+  // }
+  return NextResponse.next();
+}
 
-// export const config = {
-//   matcher: "/api/:path*", //route handlers
-// };
+export const config = {
+  //matcher: "/api/:path*", //route handlers
+};
