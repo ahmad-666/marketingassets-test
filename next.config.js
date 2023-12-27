1838c16ba7496f2a493e070ddef3eb5a74499200
ahmad-666 <ahmadrezagghz666@gmail.com>
2023-12-27 12:44:02 +0330

    feat: fix cors on POST, add OPTIONS request response , better CORS headers

--- next.config.js
+++ next.config.js
@@ -31,12 +31,12 @@ const nextConfig = {
           { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin(front url)
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
+            value: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
           },
           {
             key: "Access-Control-Allow-Headers",
             value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              "Content-Length, Content-Type, Accept, Authorization, Date, X-Api-Version",
           },
         ],
       },
