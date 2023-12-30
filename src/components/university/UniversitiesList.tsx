6c54c7f18a6d49ded8e0a37548b146bbc2d9c31c
ahmad-666 <ahmadrezagghz666@gmail.com>
2023-12-30 13:38:35 +0330

    chore: remove sharp

---
+++ src/components/university/UniversitiesList.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UniversitiesList() {
+  return (
+    <div>UniversitiesList</div>
+  )
+}
---
+++ src/types/University.ts
@@ -0,0 +1,95 @@
+import type { ServerMeta } from "./Common";
+
+//Company
+export type University = {
+  id: number;
+  name: string;
+  continent: string;
+  country: string;
+  worldRank: number;
+  countryRank: number;
+  imgSrc: string;
+};
+// export type GetCompanies = {
+//   items: Company[];
+//   totalCount: number;
+// };
+// export type Industry = {
+//   id: string;
+//   name: string;
+// };
+// export type GetIndustries = {
+//   items: Industry[];
+// };
+// export type CompanyFilters = {
+//   domain: string;
+// };
+// export type CompaniesFilters = {
+//   industry?: string;
+//   search?: string;
+//   page?: number;
+//   pageSize?: number;
+// };
+// export type CompanyResponse = {
+//   id: number;
+//   name: string;
+//   overview: string;
+//   country: string;
+//   domain: string;
+//   linkedin: string;
+//   twitter: string;
+//   facebook: string;
+//   size: string;
+//   founded: number;
+//   industry: string;
+//   followers_count: number;
+// };
+// export type GetCompanyResponse = CompanyResponse;
+// export type GetCompaniesResponse = {
+//   items: CompanyResponse[];
+// } & ServerMeta;
+// export type IndustryResponse = {
+//   industry: string;
+//   text: string;
+// };
+// export type GetIndustriesResponse = {
+//   items: IndustryResponse[];
+// };
+// //Company Comment
+// export type Comment = {
+//   id: number;
+//   date: string;
+//   userName: string;
+//   userEmail?: string;
+//   comment: string;
+//   rate: number;
+// };
+// export type GetComments = {
+//   items: Comment[];
+//   totalCount: number;
+// };
+// export type CommentFilters = {
+//   companyId: number;
+//   page?: number;
+//   pageSize?: number;
+// };
+// export type CommentReqBody = {
+//   companyId: number;
+//   userName: string;
+//   userEmail: string;
+//   body: string;
+//   rate: number;
+// };
+// export type CommentResponse = {
+//   id: number; //commentId
+//   createdAt: Date;
+//   updatedAt: Date;
+//   companyId: number;
+//   userName: string;
+//   userEmail: string;
+//   body: string;
+//   rate: number;
+// };
+// export type GetCommentsResponse = {
+//   items: CommentResponse[];
+// } & ServerMeta;
