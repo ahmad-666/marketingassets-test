b132f3d8f7819565e3d172cf0a6c9524df0f1ea8
ahmad-666 <ahmadrezagghz666@gmail.com>
2023-12-30 14:36:42 +0330

    feat: add types for University

--- src/types/University.ts
+++ src/types/University.ts
@@ -1,95 +1,200 @@
 import type { ServerMeta } from "./Common";
 
-//Company
-export type University = {
-  id: number;
+//Basic Types
+export type Type = {
+  type: string;
+  selection: string;
+  calendar: string;
+  campus: string;
+};
+export type Rank = {
+  world: number;
+  country: number;
+  admission: string;
+};
+export type Count = {
+  students: string;
+  staffs: string;
+};
+export type Pricing = {
+  localUnderGraduate: string;
+  localPostGraduate: string;
+  internationalUnderGraduate: string;
+  internationalPostGraduate: string;
+};
+export type DegreeLevel = {
+  Diploma: boolean;
+  Bachelor: boolean;
+  Master: boolean;
+  doctorate: boolean;
+};
+export type Field = {
   name: string;
+  degreeLevel: DegreeLevel;
+};
+export type Restriction = {
+  gender: string;
+  internationalStudent: boolean;
+  religious: string;
+};
+export type Features = {
+  hasHousing: boolean;
+  hasLibrary: boolean;
+  hasSportFacilities: boolean;
+  hasFinancialAid: boolean;
+  hasAbroadStudy: boolean;
+  hasDistanceLearning: boolean;
+  hasAcademicCounseling: boolean;
+  hasCareerServices: boolean;
+  hasHospital: boolean;
+};
+export type Location = {
   continent: string;
   country: string;
-  worldRank: number;
-  countryRank: number;
-  imgSrc: string;
+  countryCode: string;
+  admissionOffice: string;
 };
-// export type GetCompanies = {
-//   items: Company[];
-//   totalCount: number;
-// };
-// export type Industry = {
-//   id: string;
-//   name: string;
-// };
-// export type GetIndustries = {
-//   items: Industry[];
-// };
-// export type CompanyFilters = {
-//   domain: string;
-// };
-// export type CompaniesFilters = {
-//   industry?: string;
-//   search?: string;
-//   page?: number;
-//   pageSize?: number;
-// };
-// export type CompanyResponse = {
-//   id: number;
-//   name: string;
-//   overview: string;
-//   country: string;
-//   domain: string;
-//   linkedin: string;
-//   twitter: string;
-//   facebook: string;
-//   size: string;
-//   founded: number;
-//   industry: string;
-//   followers_count: number;
-// };
-// export type GetCompanyResponse = CompanyResponse;
-// export type GetCompaniesResponse = {
-//   items: CompanyResponse[];
-// } & ServerMeta;
-// export type IndustryResponse = {
-//   industry: string;
-//   text: string;
-// };
-// export type GetIndustriesResponse = {
-//   items: IndustryResponse[];
-// };
-// //Company Comment
-// export type Comment = {
-//   id: number;
-//   date: string;
-//   userName: string;
-//   userEmail?: string;
-//   comment: string;
-//   rate: number;
-// };
-// export type GetComments = {
-//   items: Comment[];
-//   totalCount: number;
-// };
-// export type CommentFilters = {
-//   companyId: number;
-//   page?: number;
-//   pageSize?: number;
-// };
-// export type CommentReqBody = {
-//   companyId: number;
-//   userName: string;
-//   userEmail: string;
-//   body: string;
-//   rate: number;
-// };
-// export type CommentResponse = {
-//   id: number; //commentId
-//   createdAt: Date;
-//   updatedAt: Date;
-//   companyId: number;
-//   userName: string;
-//   userEmail: string;
-//   body: string;
-//   rate: number;
-// };
-// export type GetCommentsResponse = {
-//   items: CommentResponse[];
-// } & ServerMeta;
+export type Contact = {
+  address: string;
+  tel: string;
+  fax: string;
+};
+export type Social = {
+  website: string;
+  wikipedia: string;
+  linkedin: string;
+  facebook: string;
+  twitter: string;
+  youtube: string;
+  instagram: string;
+  googleScholar: string;
+};
+//University
+export type University = {
+  id: number;
+  orgCode: string;
+  name: string;
+  imgSrc: string;
+  overview: string;
+  memberships: string[];
+  type: Type;
+  rank: Rank;
+  count: Count;
+  pricing: Pricing;
+  fields: Field[];
+  restriction: Restriction;
+  features: Features;
+  location: Location;
+  contact: Contact;
+  social: Social;
+};
+export type GetUniversities = {
+  items: University[];
+  totalCount: number;
+};
+export type UniversityFilter = {
+  name: string;
+};
+export type UniversitiesFilters = {
+  search?: string;
+  page?: number;
+  pageSize?: number;
+};
+export type UniversityResponse = {
+  id: number;
+  name: string;
+  country: string;
+  continent: string;
+  logo_name: string;
+  country_ranking: number;
+  world_ranking: number;
+  overview: string;
+  country_code: string;
+  address: string;
+  tel: string;
+  fax: string;
+  website: string;
+  gender: string;
+  international_students: boolean;
+  selection_type: string;
+  admission_rate: string;
+  admission_office: string;
+  student_enrollment: string;
+  academic_staff: string;
+  entity_type: string;
+  academic_calendar: string;
+  campus_setting: string;
+  religious_affiliation: string;
+  library: boolean;
+  housing: boolean;
+  sport_facilities: boolean;
+  financial_aids: boolean;
+  study_abroad: boolean;
+  distance_learning: boolean;
+  academic_counseling: boolean;
+  career_services: boolean;
+  institutional_hospital: boolean;
+  membership_list: string[];
+  study_degree_level: {
+    [key: string]: {
+      "Diploma Associate Foundation": boolean;
+      Bachelor: boolean;
+      Master: boolean;
+      Doctorate: boolean;
+    };
+  };
+  local_students_undergraduage: string;
+  local_students_postgraduate: string;
+  international_students_undergraduage: string;
+  international_students_postgraduate: string;
+  wikipedia_page: string;
+  instagram_page: string;
+  youtube_page: string;
+  facebook_page: string;
+  twitter_page: string;
+  linkedin_page: string;
+  google_scholar_page: string;
+  org_code: number | string;
+};
+export type GetUniversityResponse = UniversityResponse;
+export type GetUniversitiesResponse = {
+  items: UniversityResponse[];
+} & ServerMeta;
+//University Comment
+export type Comment = {
+  id: number;
+  date: string;
+  userName: string;
+  userEmail?: string;
+  comment: string;
+  rate: number;
+};
+export type GetComments = {
+  items: Comment[];
+  totalCount: number;
+};
+export type CommentFilters = {
+  universityId: number;
+  page?: number;
+  pageSize?: number;
+};
+export type CommentReqBody = {
+  universityId: number;
+  userName: string;
+  userEmail: string;
+  body: string;
+  rate: number;
+};
+export type CommentResponse = {
+  id: number; //commentId
+  createdAt: Date;
+  updatedAt: Date;
+  universityId: number;
+  userName: string;
+  userEmail: string;
+  body: string;
+  rate: number;
+};
+export type GetCommentsResponse = {
+  items: CommentResponse[];
+} & ServerMeta;
